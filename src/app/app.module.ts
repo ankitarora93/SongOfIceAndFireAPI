fe9fcaca5cd9dfe47ee6035746814365cb8a4503
Ankit Arora <arora.ankit7@gmail.com>
2018-06-16 17:58:25 +0530

    Added components books, components and houses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { CharactersComponent } from './characters/characters.component';
+import { HousesComponent } from './houses/houses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    CharactersComponent,
+    HousesComponent
   ],
   imports: [
     BrowserModule
