3e6f5b8b2273a3efbcffa78bcd55dd51f9aa70c4
Ankit Arora <arora.ankit7@gmail.com>
2018-06-17 19:23:45 +0530

    Added boostrap and jquery

--- src/app/houses/houses.component.ts
+++ src/app/houses/houses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { House } from './../house';
 import { NullAstVisitor } from '@angular/compiler';
+import { HOUSES } from './../mock-houses';
 
 @Component({
   selector: 'app-houses',
@@ -9,33 +10,7 @@ import { NullAstVisitor } from '@angular/compiler';
 })
 export class HousesComponent implements OnInit {
 
-  house: House = {
-    "name": "House Targaryen of King's Landing",
-    "region": "The Crownlands",
-    "coatOfArms": "Sable, a dragon thrice-headed gules",
-    "words": "Fire and Blood",
-    "titles": [
-      "King of the Andals, the Rhoynar and the First Men",
-      "Lord of the Seven Kingdoms",
-      "Prince of Summerhall"
-    ],
-    "seats": [
-      "Red Keep (formerly)",
-      "Summerhall (formerly)"
-    ],
-    "currentLord": null,
-    "heir": null,
-    "overlord": null,
-    "founded": "House Targaryen: >114 BCHouse Targaryen of King's Landing:1 AC",
-    "founder": null,
-    "diedOut": "",
-    "ancestralWeapons": [
-      "Blackfyre",
-      "Dark Sister"
-    ],
-    "cadetBranches": [],
-    "swornMembers": []
-  }
+  houses = HOUSES;
 
   constructor() { }
 
--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -31,5 +31,20 @@ export const BOOKS: Book[] = [
         "released": "1994-08-01T00:00:00",
         "characters": [],
         "povCharacters": []
-      }
+      }, 
+
+      {
+        "name": "A Game of Thrones",
+        "isbn": "978-0553103540",
+        "authors": [
+            "George R. R. Martin"
+        ],
+        "numberOfPages": 694,
+        "publisher": "Bantam Books",
+        "country": "United States",
+        "mediaType": "Hardcover",
+        "released": "1996-08-01T00:00:00",
+        "characters": [],
+        "povCharacters": []
+    }
 ];
